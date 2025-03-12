19ffe4b09a3cc16011e08acdf13ea5506575c587
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-03-12 12:00:31 -0700

    wip: dataset description metadata

---
+++ pysoda/core/metadata/constants.py
@@ -0,0 +1,6 @@
+from os.path import join, getsize, abspath, dirname
+
+
+
+TEMPLATE_PATH = join(dirname(abspath(__file__)), '..', 'metadata_templates')
+METADATA_UPLOAD_BF_PATH = join(dirname(abspath(__file__)), 'metadata_upload_bf')
--- pysoda/core/metadata/submission.py
+++ pysoda/core/metadata/submission.py
@@ -1,16 +1,14 @@
-from os.path import join, getsize, abspath, dirname
+from os.path import join, getsize
 from openpyxl import load_workbook
-from openpyxl.styles import PatternFill, Font
-from string import ascii_uppercase
-import itertools
+from openpyxl.styles import Font
 import shutil
 import pkg_resources
 import json
 from jsonschema import validate
 
-TEMPLATE_PATH = join(dirname(abspath(__file__)), '..', 'metadata_templates')
-METADATA_UPLOAD_BF_PATH = join(dirname(abspath(__file__)), 'metadata_upload_bf')
 
+from .constants import METADATA_UPLOAD_BF_PATH, TEMPLATE_PATH
+from .excel_utils import rename_headers, excel_columns
 
 
 
@@ -101,40 +99,7 @@ def create_excel(soda, upload_boolean, destination_path):
         # upload_metadata_file("submission.xlsx", bfaccount, bfdataset, destination, True)
     return {"size": size}
 
-def excel_columns(start_index=0):
-    """
-    NOTE: does not support more than 699 contributors/links
-    """
-    single_letter = list(ascii_uppercase[start_index:])
-    two_letter = [a + b for a, b in itertools.product(ascii_uppercase, ascii_uppercase)]
-    return single_letter + two_letter
 
-
-def rename_headers(workbook, max_len, start_index):
-  """
-  Rename header columns if values exceed 3. Change Additional Values to Value 4, 5,...
-  Adds styling to the column headers as well.
-  """
-
-  columns_list = excel_columns(start_index=start_index)
-  if max_len >= start_index:
-      workbook[columns_list[0] + "1"] = "Value"
-      for i, column in zip(range(2, max_len + 1), columns_list[1:]):
-
-          workbook[column + "1"] = f"Value {str(i)}"
-          cell = workbook[column + "1"]
-
-          blueFill = PatternFill(
-              start_color="9CC2E5", end_color="9CC2E5", fill_type="solid"
-          )
-
-          font = Font(bold=True)
-          cell.fill = blueFill
-          cell.font = font
-
-  else:
-      delete_range = len(columns_list) - max_len
-      workbook.delete_cols(4 + max_len, delete_range)
 
 
 
