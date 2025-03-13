9f618a053fda0068317f64dba44994a0ae787d4c
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-03-13 15:22:59 -0700

    feat: readme changes in pysoda

--- pysoda/core/metadata/__input__.py
+++ pysoda/core/metadata/__input__.py
@@ -1,2 +1,3 @@
 from .submission import create_excel
 from .dataset_description import create_excel
+from .readme_changes import create_excel
---
+++ pysoda/core/metadata/readme_changes.py
@@ -0,0 +1,23 @@
+from .constants import METADATA_UPLOAD_BF_PATH
+from os.path import join, getsize
+
+
+# this function saves and uploads the README/CHANGES to Pennsieve, just when users choose to generate onto Pennsieve
+## (not used for generating locally)
+def create_excel(soda, file_type):
+    file_path = join(METADATA_UPLOAD_BF_PATH, file_type)
+    text_string = ''
+
+    if file_type == "README.txt":
+        text_string = soda["dataset_metadata"]["README"]
+    else:
+        text_string = soda["dataset_metadata"]["CHANGES"]
+
+    with open(file_path, "w") as f:
+        f.write(text_string)
+
+    size = getsize(file_path)
+
+    # upload_metadata_file(file_type, bfaccount, bfdataset, file_path, True)
+
+    return { "size": size, "filepath": file_path }
