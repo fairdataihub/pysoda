9c8cbf36f69b32464e551f45909c8e1c57c4ca47
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-03-24 13:42:29 -0700

    feat: upload performances file to Pennsieve

--- pysoda/core/metadata/performances.py
+++ pysoda/core/metadata/performances.py
@@ -5,14 +5,12 @@ from os.path import join, getsize
 from openpyxl import load_workbook
 import shutil
 from ...utils import validate_schema
+from .helpers import upload_metadata_file
 
-def create_excel(soda, upload_boolean, filepath):
+def create_excel(soda, upload_boolean, local_destination):
     source = join(TEMPLATE_PATH, SDS_FILE_PERFORMANCES)
 
-    if upload_boolean:
-        destination = join(METADATA_UPLOAD_PS_PATH, SDS_FILE_PERFORMANCES)
-    else:
-        destination = filepath
+    destination = join(METADATA_UPLOAD_PS_PATH, SDS_FILE_PERFORMANCES) if upload_boolean else local_destination
 
     shutil.copyfile(source, destination)
 
@@ -22,7 +20,6 @@ def create_excel(soda, upload_boolean, filepath):
     performances = soda["dataset_metadata"]["performances"]
 
     validate_schema(performances, SCHEMA_NAME_PERFORMANCES)
-
 
     # get the ascii column headers
     row = 2
@@ -34,38 +31,45 @@ def create_excel(soda, upload_boolean, filepath):
         ws1[ascii_headers[3] + str(row)] = performance.get("start_datetime", "")
         ws1[ascii_headers[4] + str(row)] = performance.get("end_datetime", "")
         participants = " ".join(performance.get("participants", []))
-        print(participants)
         ws1[ascii_headers[5] + str(row)] = participants
         ws1[ascii_headers[6] + str(row)] = performance.get("additional_metadata", "")
         row += 1
 
     wb.save(destination)
 
+    size = getsize(destination)
 
-soda = {
-    "dataset_metadata": {
-        "performances": [
-            {
-                "performance_id": "P001",
-                "protocol_url_or_doi": "https://doi.org/10.1234/abcd",
-                "date": "2023-10-01",
-                "start_datetime": "2023-10-01T10:00:00Z",
-                "end_datetime": "2023-10-01T12:00:00Z",
-                "participants": ["sub-1", "sub-2"],
-                "additional_metadata": "unfun metadata here"
-            },
-            {
-                "performance_id": "P002",
-                "protocol_url_or_doi": "https://doi.org/10.1234/abcde",
-                "date": "2023-10-01",
-                "start_datetime": "2023-10-01T10:00:00Z",
-                "end_datetime": "2023-10-01T12:00:00Z",
-                "participants": ["sub-1", "sub-2"],
-                "additional_metadata": "fun metadata here"
-            }
-        ]
-    }
-}
+    ## if generating directly on Pennsieve, call upload function
+    if upload_boolean:
+        upload_metadata_file(SDS_FILE_PERFORMANCES, soda,  destination, True)
+
+    return {"size": size}
 
 
-create_excel(soda, False, "performances.xlsx")
+# soda = {
+#     "dataset_metadata": {
+#         "performances": [
+#             {
+#                 "performance_id": "P001",
+#                 "protocol_url_or_doi": "https://doi.org/10.1234/abcd",
+#                 "date": "2023-10-01",
+#                 "start_datetime": "2023-10-01T10:00:00Z",
+#                 "end_datetime": "2023-10-01T12:00:00Z",
+#                 "participants": ["sub-1", "sub-2"],
+#                 "additional_metadata": "unfun metadata here"
+#             },
+#             {
+#                 "performance_id": "P002",
+#                 "protocol_url_or_doi": "https://doi.org/10.1234/abcde",
+#                 "date": "2023-10-01",
+#                 "start_datetime": "2023-10-01T10:00:00Z",
+#                 "end_datetime": "2023-10-01T12:00:00Z",
+#                 "participants": ["sub-1", "sub-2"],
+#                 "additional_metadata": "fun metadata here"
+#             }
+#         ]
+#     }
+# }
+
+
+# create_excel(soda, False, "performances.xlsx")
