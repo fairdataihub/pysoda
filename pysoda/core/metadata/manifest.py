38c92c05bc1e2b9649a8f2900da82de0e57d6574
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-03-26 15:12:40 -0700

    feat: generate manifest files locally and to Pennsieve

---
+++ pysoda/core/metadata/manifest.py
@@ -0,0 +1,152 @@
+from .constants import METADATA_UPLOAD_PS_PATH, TEMPLATE_PATH, SDS_FILE_MANIFEST, SCHEMA_NAME_MANIFEST
+from .excel_utils import rename_headers, excel_columns
+from openpyxl.styles import PatternFill
+from os.path import join, getsize
+from openpyxl import load_workbook
+import shutil
+from ...utils import validate_schema
+from .helpers import upload_metadata_file
+
+
+def create_excel(soda, upload_boolean, local_destination):
+    source = join(TEMPLATE_PATH, SDS_FILE_MANIFEST)
+
+    destination = join(METADATA_UPLOAD_PS_PATH, SDS_FILE_MANIFEST) if upload_boolean else local_destination
+
+    shutil.copyfile(source, destination)
+
+    wb = load_workbook(destination)
+    ws1 = wb["Sheet1"]
+
+    manifest = soda["dataset_metadata"]["manifest_files"]
+
+    validate_schema(manifest, SCHEMA_NAME_MANIFEST)
+
+    # get the ascii column headers
+    row = 2
+    ascii_headers = excel_columns(start_index=0)
+    manifest_entries = manifest["data"]["data"]
+    for entry_items in manifest_entries:
+      ascii_header_idx = 0 
+      for entry in entry_items:
+        ws1[ascii_headers[ascii_header_idx] + str(row)] = entry
+        ascii_header_idx += 1
+      row += 1
+
+    wb.save(destination)
+
+    size = getsize(destination)
+
+    ## if generating directly on Pennsieve, call upload function
+    if upload_boolean:
+        upload_metadata_file(SDS_FILE_MANIFEST, soda,  destination, True)
+
+    return {"size": size}
+
+
+# soda = {
+#     "generate-dataset": {
+#         "destination": "local",
+#         "generate-option": "new",
+#         "dataset-name": "test_dataset",
+#         "path": "/Users/aaronm",
+#         "if-existing": ""
+#     },
+#     "dataset-structure": {
+#         "folders": {
+#             "primary": {
+#                 "folders": {},
+#                 "files": {
+#                     "validation_progress.txt": {
+#                         "location": "local",
+#                         "path": "/Users/aaronm/gmps-11/primary/pool-1/validation_progress.txt",
+
+#                         "action": ["new"],
+#                     },
+#                     "clean_metadata.py": {
+#                         "location": "local",
+
+#                         "path": "/Users/aaronm/gmps-11/primary/pool-1/sub-1/clean_metadata.py",
+#                         "action": ["new"]
+#                     }
+#                 }
+#             }
+#         }, 
+#         "files":  {
+#             "metadata.xlsx": {
+#                 "location": "local",
+#                 "path": "/Users/aaronm/gmps-11/primary/metadata.xlsx",
+
+#                 "action": ["new"]
+#             }
+#         },
+#         "relativePath": "/"
+#     },
+#     "dataset_metadata": {
+#       "manifest_files": {
+#           "data": {
+#               "headers": [                  
+#                     "filename",
+#                     "timestamp",
+#                     "description",
+#                     "file type",
+#                     "entity",
+#                     "data modality",
+#                     "also in dataset",
+#                     "also in dataset path",
+#                     "data dictionary path",
+#                     "entity is transitive",
+#                     "Additional Metadata"
+#               ],
+#               "data": [
+#                   [
+#                       "metadata.xlsx",
+#                       "",
+#                       "",
+#                       ".xlsx",
+#                       "subject-1",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       ""
+#                   ],
+#                   [
+#                       "validation_progress.txt",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       ""
+#                   ],
+#                   [
+#                       "clean_metadata.py",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       "",
+#                       ""
+#                   ]
+#               ]
+#           }
+#       }
+#     }
+# }
+
+
+# try:
+#   create_excel(soda, False, "manifest.xlsx")
+# except Exception as e:
+#   print(e)
+
