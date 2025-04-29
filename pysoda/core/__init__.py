5bfe1c0922702c58d3b5998292c8359b5c8f89fa
aaronm-2112 <aaron.marroquin96@gmail.com>
2025-04-29 10:18:31 -0700

    refactor: create one root logger in core without handler

--- pysoda/core/__init__.py
+++ pysoda/core/__init__.py
@@ -6,11 +6,5 @@ from .permissions import has_edit_permissions
 
 # Create a logger for the package
 logger = logging.getLogger(__name__)
-
 # Optional: Provide a default configuration if no handlers are set
-if not logger.hasHandlers():
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.WARNING)
+logger.setLevel(logging.WARNING)
